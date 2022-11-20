18f91f426b9f5a9464cb06952f1351d8cc50f368
Nomsku <albin.sihvola@metropolia.fi>
2022-11-20 19:14:35 +0200

    utils

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 'use strict';
 const express = require('express');
 const app = express();
+const passport = require('./utils/pass');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const port = 3000;
+
+const loggedIn = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/form');
+  }
+};
 
 const username = 'foo';
 const password = 'bar';
@@ -14,6 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 app.use(session({ secret: 'jumanji'}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('views', './views');
 app.set('view engine', 'pug');
@@ -44,23 +55,17 @@ app.get('/form', (req, res) => {
   res.render('form');
 });
 
-app.get('/secret', (req, res) => {
-  if (req.session.logged) {
-    res.render('secret');
-  } else {
-    res.redirect('/form');
-  }
+app.get('/secret', loggedIn, (req, res) => {
+  res.render('secret');
 });
 
-app.post('/login', (req, res) => {
-  if (req.body.password === password && req.body.username === username) {
-    req.session.logged = true;
-    res.redirect('/secret');
-  } else {
-    req.session.logged = false;
-    res.redirect('/form');
-  }
-});
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/form'}),
+    (req, res) => {
+      console.log('success');
+      res.redirect('/secret');
+    });
+
 
 app.get('/logout', (req, res) => {
   req.session.destroy();
